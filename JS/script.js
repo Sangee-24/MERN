b873aa37d347f0eb1d2a87d27dab6fa67a805452
Sangee <sangeerani24@gmail.com>
2025-03-17 12:39:06 +0530

    updated

---
+++ JS/script.js
@@ -0,0 +1,49 @@
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+let name="sangee"
+let number="true"
+console.log(name)
+var val=10
+console.log(val)
+var a=10 //global
+{
+    let b=10 //local
+}
+console.log(a)
+
+const pi=3.2
+
+let x=20
+let y=30
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+
+let z=x++
+console.log(z)
+console.log(x)
+
+let num=42
+let bool=true
+let s="sangee"
+let n=null
+console.log(num,bool,s,n)
+//parameters
+function add(a,b){
+    console.log(a+b)
+}
+add(10,20)//arguments
+add(39,42)
+
+let q=100
+if(q>50){
+    console.log("true")
+}
+else{
+    console.log("False")
+}
+for(let i=0;i<10;i++){
+    console.log(i)
+}
